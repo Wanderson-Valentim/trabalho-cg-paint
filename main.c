fbbebd76ded612329d5dc04a92a58417d849739d
Wanderley MBF <70535956+derleymad@users.noreply.github.com>
2023-04-20 11:11:41 -0300

    criando menu

--- main.c
+++ main.c
@@ -3,38 +3,64 @@
 #include <GL/glut.h>
 #include <math.h>
 
-// Coloca as funções de inicialização
-// Inicializa os parâmetros do rendering
-int init(void){
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+// Funções a serem feitas
+void drawCircle() {}
+
+void drawPoligono(){}
+
+void criaLinha(){}
+
+void rotacionar(){}
+
+void transladar(){}
+
+void eventosDoMenu(int option) {
+    switch(option) {
+        case 1:
+            //criar linha
+            break;
+        case 2:
+            //circulo
+            break;
+        case 3:
+            //poligono
+            break;
+        case 4:
+            //rotacionar
+            break;
+        case 5:
+            //transladar 
+            break;
+    }
 }
 
-void display(void){
+void criarMenu() {
+    int menu_id = glutCreateMenu(eventosDoMenu);
+    glutAddMenuEntry("Linha", 1);
+    glutAddMenuEntry("Circulo", 2);
+    glutAddMenuEntry("Poligono", 3);
+    glutAddMenuEntry("Rotacionar objeto", 4);
+    glutAddMenuEntry("Transladar objeto", 5);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-
     glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_POLYGON);
-        glVertex2i(10, 140);
-        glVertex2i(190, 140);
-        glVertex2i(190, 10);
-        glVertex2i(10, 10);
-    glEnd();
-
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     glFlush();
 }
 
-int main(int argc, char** argv){
-    glutInit(&argc,argv); //inicializa o GLUT
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // configura o modo de display
-    glutInitWindowSize(400, 300); // configura a largura e altura da janela de exibição
-    glutInitWindowPosition(200, 200); // posição da janela na tela
-    glutCreateWindow("ATIVIDADE 2 CG"); // cria a janela de exibição
-
-    init();
-    glutDisplayFunc(display); // estabele a função "display como a função callback de exibição"
-    glutMainLoop(); // mostre tudo e espere
-
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Paint bugado");
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    criarMenu();
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
+
