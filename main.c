8a61c1f1bd00dca0a2677570a02d252da4731a2c
Wanderson Valentim <wanderson200108@gmail.com>
2023-04-25 23:46:53 -0300

    create points, lines and polygons

--- main.c
+++ main.c
@@ -8,8 +8,6 @@
 #define MAX_POLYGONS 30
 #define MAX_POINTS_POLYGON 10
 
-int menuOption;
-
 /* ------------- Definindo estrutura dos objetos -------------*/
 typedef struct{
     float x;
@@ -23,7 +21,17 @@ typedef struct{
 
 typedef struct{
     Point points[MAX_POINTS_POLYGON];
+    int numPoints;
 }Polygon;
+
+/* ------------- Definindo variaveis globais -------------*/
+int numClicksToDrawLine = 0;
+Point clicksToDrawLine[2];
+
+int menuOption = 1;
+int menuClick = 0;
+
+int blockPolygonCreation = 0;
 
 /* ------------- Definindo variaveis dos objetos -------------*/
 int numLines = 0;
@@ -44,13 +52,13 @@ void addPoint(float x, float y){
         Point point;
         point.x = x;
         point.y = y;
-        lines[numLines] = point;
+        points[numPoints] = point;
         numPoints += 1;
     }
 }
 
 void removePoint(float x, float y){
-    if(numPoints == 0){
+    /*if(numPoints == 0){
         // op. invalida fzr alguma coisa
     }
     else{
@@ -64,27 +72,44 @@ void removePoint(float x, float y){
             for(int i = indexElement; i < numPoints-1; i++){
                 points[indexElement] = points[indexElement+1];
             }
+
+            numPoints -= 1;
         }
-        numPoints -= 1;
-    }
+    }*/
 }
 
-void addLine(){
+void addLine(Point start, Point end){
     if(numLines == MAX_LINES){
         // op. invalida fzr alguma coisa
     }
     else{
+        Line line;
+        line.start = start;
+        line.end = end;
+        lines[numLines] = line;
         numLines += 1;
     }
 }
 
-void removeLine(){
-    if(numLines == 0){
+void removeLine(Point start, Point end){
+    /*if(numLines == 0){
         // op. invalida fzr alguma coisa
     }
     else{
-        numLines -= 1;
-    }
+        int indexElement = 0
+
+        while(indexElement < numLines || ()){
+            indexElement++;
+        }
+
+        if(indexElement < numLines){
+            for(int i = indexElement; i < numLines-1; i++){
+                lines[indexElement] = points[indexElement+1];
+            }
+
+            numLines -= 1;
+        }
+    }*/
 }
 
 void addPolygon(){
@@ -92,8 +117,16 @@ void addPolygon(){
         // op. invalida fzr alguma coisa
     }
     else{
+        Polygon polygon;
+        polygon.numPoints = 0;
         numPolygons += 1;
     }
+}
+
+void addPointOnPolygon(Point point){
+    int numPointsPolygon = polygons[numPolygons-1].numPoints;
+    polygons[numPolygons-1].points[numPointsPolygon] = point;
+    polygons[numPolygons-1].numPoints += 1;
 }
 
 void removePolygon(){
@@ -101,28 +134,47 @@ void removePolygon(){
         // op. invalida fzr alguma coisa
     }
     else{
-        numPolygons -= 1;
+        //numPolygons -= 1;
     }
 }
 
 /* ------------- Funções de matrizes -------------*/
-void multiplyMatrix(int matrix1[][], int matrix2[][], int result[][]) {
+/*void multiplyMatrix(int matrix1[][], int matrix2[][], int result[][]) {
 
-}
+}*/
 
 void sumMatrix(){
 }
 
 /* ------------- Funções de desenho de objetos -------------*/
-void drawPoint() {
-
+void drawPoints() {
+    glColor3f(0.0, 1.0, 1.0);
+    glBegin(GL_POINTS);
+        for(int i = 0; i < numPoints; i++){
+            glVertex2f(points[i].x, 582 - points[i].y);
+        }
+    glEnd();
 }
 
-void drawPolygon(){
-
+void drawLines(){
+    glColor3f(1.0, 1.0, 0.0);
+    for(int i = 0; i < numLines; i++){
+        glBegin(GL_LINES);
+            glVertex2f(lines[i].start.x, 582 - lines[i].start.y);
+            glVertex2f(lines[i].end.x, 582 - lines[i].end.y);
+        glEnd();
+    }
 }
 
-void drawLine(){
+void drawPolygons(){
+    glColor3f(0.0, 1.0, 0.0);
+    for(int i = 0; i < numPolygons; i++){
+        glBegin(GL_POLYGON);
+            for(int j = 0; j < polygons[i].numPoints; j++){
+                glVertex2f(polygons[i].points[j].x, 582 - polygons[i].points[j].y);
+            }
+        glEnd();
+    }
 }
 
 void rotate(){
@@ -137,34 +189,67 @@ void scale(){
 /* ------------- Funções de eventos -------------*/
 void menuEvents(int option) {
     menuOption = option;
-    switch(option) {
-        case 1:
-            //criar ponto
-            break;
-        case 2:
-            //criar linha
-            break;
-        case 3:
-            //poligono
-            break;
-        case 4:
-            //rotacionar
-            break;
-        case 5:
-            //transladar
-            break;
-        case 6:
-            //escala
-            break;
-        case 7:
-            //selecao
-            break;
-    }
+    menuClick = 1;
 }
 
 void mouseEvents(int button, int state, int x, int y){
-    printf("%d %d\n", button, menuOption);
-    fflush(stdout); // força a saída do buffer de stdout para poder usar o printf em tempo real
+    float convertedX = x;
+    float convertedY = y;
+
+    if(menuClick != 1){
+        switch(menuOption) {
+            case 1:
+                if(button == GLUT_LEFT_BUTTON && state == GLUT_UP){
+                    addPoint(convertedX, convertedY);
+                }
+                break;
+            case 2:
+                if(button == GLUT_LEFT_BUTTON && state == GLUT_UP){
+                    Point point;
+                    point.x = convertedX;
+                    point.y = convertedY;
+                    clicksToDrawLine[numClicksToDrawLine] = point;
+                    numClicksToDrawLine += 1;
+
+                    if(numClicksToDrawLine == 2){
+                        addLine(clicksToDrawLine[0], clicksToDrawLine[1]);
+                        numClicksToDrawLine = 0;
+                    }
+                }
+                break;
+            case 3:
+                if(button == GLUT_LEFT_BUTTON && state == GLUT_UP){
+                    if(blockPolygonCreation == 0){
+                        addPolygon();
+                        blockPolygonCreation = 1;
+                    }
+                    Point point;
+                    point.x = convertedX;
+                    point.y = convertedY;
+                    addPointOnPolygon(point);
+
+                }
+                if(button == GLUT_MIDDLE_BUTTON && state == GLUT_UP){
+                    blockPolygonCreation = 0;
+                }
+                break;
+            case 4:
+                //rotacionar
+                break;
+            case 5:
+                //transladar
+                break;
+            case 6:
+                //escala
+                break;
+            case 7:
+                //selecao
+                break;
+        }
+    }
+    /*printf("%d %d %d\n", button, menuOption, numPoints);
+    fflush(stdout); // força a saída do buffer de stdout para poder usar o printf em tempo real*/
+    menuClick = 0;
     glutPostRedisplay();
 }
 
@@ -187,18 +272,25 @@ void createMenu() {
 int init(void){
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+    gluOrtho2D(0.0, 960.0, 0.0, 582.0);
 }
 
 void display(void){
     glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    drawPoints();
+    drawLines();
+    drawPolygons();
+
     glutSwapBuffers();
 }
 
 int main(int argc, char** argv){
     glutInit(&argc,argv); //inicializa o GLUT
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // configura o modo de display
-    glutInitWindowSize(500, 500); // configura a largura e altura da janela de exibição
+    glutInitWindowSize(960, 582); // configura a largura e altura da janela de exibição
     glutInitWindowPosition(200, 200); // posição da janela na tela
     glutCreateWindow("Paint bugado"); // cria a janela de exibição
 
